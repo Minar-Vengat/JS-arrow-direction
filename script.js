e7412a1b5ef2a29e6c03fcedf0b53af1abac2d1d
minar vengat <9750042828minararun@gmail.com>
2025-03-17 09:42:05 +0530

    complete

---
+++ script.js
@@ -0,0 +1,55 @@
+document.getElementById('moveButton').addEventListener('click', function() {
+    let rocket = document.getElementById('moveButton');
+
+    rocket.classList.remove('animate');
+    void rocket.offsetWidth;
+    rocket.classList.add('animate');
+});
+
+document.getElementById('buttonONE').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonONE');
+
+    rocket.classList.remove('animated');
+    void rocket.offsetWidth;
+    rocket.classList.add('animated');
+});
+
+document.getElementById('buttonTwo').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonTwo');
+
+    rocket.classList.remove('animate');
+    void rocket.offsetWidth;
+    rocket.classList.add('animate');
+});
+
+document.getElementById('buttonThree').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonThree');
+
+    rocket.classList.remove('animated');
+    void rocket.offsetWidth;
+    rocket.classList.add('animated');
+});
+
+document.getElementById('buttonFour').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonFour');
+
+    rocket.classList.remove('animate');
+    void rocket.offsetWidth;
+    rocket.classList.add('animate');
+});
+
+document.getElementById('buttonFive').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonFive');
+
+    rocket.classList.remove('animated');
+    void rocket.offsetWidth;
+    rocket.classList.add('animated');
+});
+
+document.getElementById('buttonSix').addEventListener('click', function() {
+    let rocket = document.getElementById('buttonSix');
+
+    rocket.classList.remove('animate');
+    void rocket.offsetWidth;
+    rocket.classList.add('animate');
+});
